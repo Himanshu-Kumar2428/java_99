7a791f339c406c4b6a794f8650f0a92ec44621a2
Himanshu Kumar <himanshu.2428cseaiml@kiet.edu>
2025-08-21 23:59:47 +0530

    add java codes

--- src/Main1.java
+++
@@ -1,9 +0,0 @@
-public class Main1{
-    public static void main(String[] args){
-        pen pen1=new pen();
-        pen1.color="blue";
-        pen1.type="gel";
-
-        pen1.printColor();
-    }
-}
