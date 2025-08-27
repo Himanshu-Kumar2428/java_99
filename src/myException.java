3fe5adc255f50a8df96d160372ded464ba20a511
Himanshu Kumar <himanshu.2428cseaiml@kiet.edu>
2025-08-27 14:21:08 +0530

    Exception 27aug

--- src/myException.java
+++ src/myException.java
@@ -1,34 +1,39 @@
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class myException {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         int x,y,z=0;
-        Scanner sc=new Scanner(System.in);
-        try
-        {
-         x=sc.nextInt();
-         y=sc.nextInt();
-         z=x/y;
-            System.out.println("Exception Not Occur");
-        }
-        catch(ArithmeticException e)
-        {
-            System.out.println(e.getMessage());
-        }
-
-
-        catch(InputMismatchException e)
-        {
-            System.out.println(e);
-        }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        finally
-        {
-            System.out.println("Result is " + z);
-        }
+        DataInputStream dis=new DataInputStream(System.in);
+//        Scanner sc=new Scanner(System.in);
+//        try
+//        {
+           x=dis.readInt();
+           y=dis.readInt();
+//         x=sc.nextInt();
+//         y=sc.nextInt();
+           z=x/y;
+           System.out.println("Exception Not Occur");
+//        }
+//        catch(ArithmeticException e)
+//        {
+//            System.out.println(e.getMessage());
+//        }
+//
+//
+//        catch(InputMismatchException e)
+//        {
+//            System.out.println(e);
+//        }
+//        catch(Exception e)
+//        {
+//            System.out.println(e.getMessage());
+//        }
+//        finally
+//        {
+//            System.out.println("Result is " + z);
+//        }
 
 
     }
